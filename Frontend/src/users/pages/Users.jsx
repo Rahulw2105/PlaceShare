dfe9adeb9c2f564dfbdb5b64de840b9e209c74f6
Rahulw2105 <wankhederahul116@gmail.com>
2024-12-14 20:00:40 +0530

    create Dummy users array

--- Frontend/src/users/pages/Users.jsx
+++ Frontend/src/users/pages/Users.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import UsersList from "../components/UsersList";
+import userImg from "../../assets/user.jpg";
 const Users = () => {
-  return <h1> Users Worls</h1>;
+  const USERS = [
+    {
+      ud: "u1",
+      name: "Rahul Wankhede",
+      image: userImg,
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
